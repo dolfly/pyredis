2ae30ef8f2e9de9bdee3fbb31a5a65207678dda3
Ouertani Mohammed Amine <tsl.hacker@gmail.com>
2012-06-02 06:59:44 -0700

    some style

--- src/redis_wrap.py
+++ src/redis_wrap.py
@@ -5,12 +5,15 @@ class redis_wrap:
     connection = None
     def __init__(self,server_name):
         self.connection = redis_connection(server_name)
+
     def connect(self):
         self.connection.connect()
+
     def ping_server (self):
         self.connection.send("PING")
         response = self.connection.recive()
         return response.decode_reponse() == "PONG"
+
     def execute_command (self,command):
         self.connection.send(command)
         return self.connection.recive()
