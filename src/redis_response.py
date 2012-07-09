ec57a8803fc94cd08b582bd6e90ae43ade1a7bdd
Ouertani Mohammed Amine <tsl.hacker@gmail.com>
2012-07-09 14:50:49 +0100

    removed comment git push

--- src/redis_response.py
+++ src/redis_response.py
@@ -25,17 +25,6 @@ class redis_response :
         return text
 
     def response_type (self):
-        '''
-        Replies
-
-        Redis will reply to commands with different kinds of replies.
-        It is possible to check the kind of reply from the first byte sent by the server:
-        With a single line reply the first byte of the reply will be '+'
-        With an error message the first byte of the reply will be '-'
-        With an integer number the first byte of the reply will be ':'
-        With bulk reply the first byte of the reply will be '$'
-        With multi-bulk reply the first byte of the reply will be '*'
-        '''
         reply_types = {"+"  : "single line",
                        "-"  : "error",
                        ":"  : "integer",
